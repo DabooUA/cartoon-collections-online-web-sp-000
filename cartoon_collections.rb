c81ddbbfcee7f86a27a49d8e8ebe965fe6a9240d
Serhiy Bardysh <sbardysh@tutanota.com>
2020-05-27 01:32:43 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,7 +2,7 @@ def roll_call_dwarves (dwarves)
 	 i = 0
    new_list_of_dwarves = []
 	 while i < dwarves.length
-	 new_list_of_dwarves << yield(dwarves[i])
+	 new_list_of_dwarves << dwarves[i]
    i += 1
 	 end
 	new_list_of_dwarves
