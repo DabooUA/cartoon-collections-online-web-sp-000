6667cd029bd04db6de8984da635c0fb55b02ea33
Serhiy Bardysh <sbardysh@tutanota.com>
2020-05-27 01:31:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,11 +1,11 @@
-def roll_call_dwarves (collection)
+def roll_call_dwarves (dwarves)
 	 i = 0
-   block_return_values = []
-	 while i < collection.length
-	 block_return_values << yield(collection[i])
+   new_list_of_dwarves = []
+	 while i < dwarves.length
+	 new_list_of_dwarves << yield(dwarves[i])
    i += 1
 	 end
-	puts block_return_values
+	new_list_of_dwarves
 end
 
 def summon_captain_planet# code an argument here
